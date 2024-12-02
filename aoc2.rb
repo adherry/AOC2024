be5f4bc5bc5631a2e718b4530a95c85b5b11f68b
adherry <github@sven.adherry.de>
2024-12-02 12:09:50 +0100

    Fix whitespace

--- aoc2.rb
+++ aoc2.rb
@@ -3,27 +3,27 @@ measurements = []
 valid_lines = 0
 f = File.read(inputfile)
 f.each_line do |line|
- values = line.split(" ")
- measurements.push(values.map{|e| e.to_i})
+  values = line.split(" ")
+  measurements.push(values.map{|e| e.to_i})
 end
 
 def within_tolerance?(line)
-    line.each_with_index do |value, index|
-        next if index == 0
-        difference = (value - line[index-1]).abs
-        if difference > 3 || difference == 0
-          return false
-        end
+  line.each_with_index do |value, index|
+    next if index == 0
+    difference = (value - line[index-1]).abs
+    if difference > 3 || difference == 0
+      return false
     end
-    return true
+  end
+  return true
 end
 
 def is_sorted?(measurement)
-    if ((measurement == measurement.sort) || (measurement.reverse == measurement.sort))
-        true
-    else
-        false
-    end
+  if ((measurement == measurement.sort) || (measurement.reverse == measurement.sort))
+    true
+  else
+    false
+end
 end
 
 def is_safe?(line)
@@ -31,15 +31,15 @@ def is_safe?(line)
 end
 
 measurements.each do |measurement|
-    if is_safe?(measurement)
-        valid_lines += 1 
-    else
-        measurement.combination(measurement.length - 1).each do |permutation|
-            if is_safe?(permutation)
-                valid_lines += 1
-                break
-            end
-        end
+  if is_safe?(measurement)
+    valid_lines += 1
+  else
+    measurement.combination(measurement.length - 1).each do |permutation|
+      if is_safe?(permutation)
+        valid_lines += 1
+      break
+      end
     end
+  end
 end
 puts valid_lines
